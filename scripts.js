1e5528901a17c5a0a290504c916dec17c9d4ef29
Mashaun18 <animashaunoreoluwa@gmail.com>
2023-12-25 00:34:59 +0100

    add new file

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode')
+}
+
+document.getElementById('toggle-dark-mode').addEventListener('click', toggleDarkMode)
+
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault()
+    const formData = new FormData(event.target);
+    for (const [name, value] of formData) {
+      console.log(`${name}: ${value}`)
+    }
+    event.target.reset()
+  })
+  
+  
+  
+  
+  
+  
